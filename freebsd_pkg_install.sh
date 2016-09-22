ecfc8edf3908f7a1a9a6f5ff446a45ea08371342
Dinoja Padmanabhan <dinoja.padman@gmail.com>
2016-09-22 05:01:14 -0700

    Update freebsd pkg installer

--- freebsd_pkg_install.sh
+++ freebsd_pkg_install.sh
@@ -6,3 +6,4 @@ pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packag
 pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/screen-4.0.3_14.tbz
 pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/bash-4.2.42.tbz
 pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/ccache-3.1.9.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/libtool-2.4.2.tbz
