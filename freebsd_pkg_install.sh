7f210b927eb2d5ea4541d399c5dd313a1a6ad8d2
Dinoja Padmanabhan <dinoja.padman@gmail.com>
2016-09-22 03:02:18 -0700

    Add a new bashrc, freebsdrc and freebsd pkg installer

---
+++ freebsd_pkg_install.sh
@@ -0,0 +1,8 @@
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/postgresql-server-9.2.4.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/cscope-15.8a.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/vim-7.3.669_1.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/git-1.8.2.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/subversion-1.7.9_1.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/screen-4.0.3_14.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/bash-4.2.42.tbz
+pkg_add -r http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/ports/amd64/packages-8.4-release/All/ccache-3.1.9.tbz
